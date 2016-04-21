e6dfb0a06f06cc2eb2baa5ca1a7e06b5b6281b30
Ionel Cristian Mărieș <contact@ionelmc.ro>
2016-04-21 22:41:15 +0300

    Fix error reporting a bit.

--- src/holdup/cli.py
+++ src/holdup/cli.py
@@ -24,22 +24,6 @@ from contextlib import closing
 from time import time
 
 
-parser = argparse.ArgumentParser(
-    usage='%(prog)s [-h] [-f] [-t SECONDS] [-n] [service [service ...]] [-- command [arg [arg ...]]]',
-    description="Wait for services to be ready and optionally exec command."
-)
-parser.add_argument('service', nargs=argparse.ZERO_OR_MORE,
-                    help='A service to wait for. '
-                         'Supported protocols: "tcp://host:port/", "unix:///path/to/domain.sock".')
-parser.add_argument('command', nargs=argparse.OPTIONAL,
-                    help='An optional command to exec.')
-parser.add_argument('-t', '--timeout', metavar='SECONDS', type=float, default=5.0,
-                    help='Time to wait for services to be ready. Default: %(default)s')
-parser.add_argument('-n', '--no-abort', action='store_true',
-                    help='Ignore failed services. '
-                         'This makes `holdup` return 0 exit code regardless of services actually responding.')
-
-
 class TcpCheck(object):
     def __init__(self, host, port):
         self.host = host
@@ -71,19 +55,21 @@ class UnixCheck(object):
 
 def parse_service(service):
     if '://' not in service:
-        parser.error('Invalid service spec %r. Must have "://".' % service)
+        raise argparse.ArgumentTypeError('Invalid service spec %r. Must have "://".' % service)
     proto, value = service.split('://', 1)
 
     if proto == 'tcp':
         if ':' not in value:
-            parser.error('Invalid service spec %r. Must have ":". Where\'s the port?' % service)
-        host, port = value.split(':', 1)
+            raise argparse.ArgumentTypeError('Invalid service spec %r. Must have ":". Where\'s the port?' % service)
+        host, port = value.strip('/').split(':', 1)
         if not port.isdigit():
-            parser.error('Invalid service spec %r. Port must be a number.' % service)
+            raise argparse.ArgumentTypeError('Invalid service spec %r. Port must be a number.' % service)
         port = int(port)
         return TcpCheck(host, port)
     elif proto == 'unix':
         return UnixCheck(value)
+    else:
+        raise argparse.ArgumentTypeError('Unknown protocol in %r. Must be "tcp" or "unix".' % service)
 
 
 def filter_passing(checks):
@@ -94,6 +80,22 @@ def filter_passing(checks):
         except Exception as exc:
             print(check, exc)
             yield check
+
+
+parser = argparse.ArgumentParser(
+    usage='%(prog)s [-h] [-f] [-t SECONDS] [-n] [service [service ...]] [-- command [arg [arg ...]]]',
+    description="Wait for services to be ready and optionally exec command."
+)
+parser.add_argument('service', nargs=argparse.ONE_OR_MORE, type=parse_service,
+                    help='A service to wait for. '
+                         'Supported protocols: "tcp://host:port/", "unix:///path/to/domain.sock".')
+parser.add_argument('command', nargs=argparse.OPTIONAL,
+                    help='An optional command to exec.')
+parser.add_argument('-t', '--timeout', metavar='SECONDS', type=float, default=5.0,
+                    help='Time to wait for services to be ready. Default: %(default)s')
+parser.add_argument('-n', '--no-abort', action='store_true',
+                    help='Ignore failed services. '
+                         'This makes `holdup` return 0 exit code regardless of services actually responding.')
 
 
 def main():
@@ -112,7 +114,7 @@ def main():
     else:
         argv, command = sys.argv[1:], None
     options = parser.parse_args(args=argv)
-    pending = [parse_service(service) for service in options.service]
+    pending = list(options.service)
     start = time()
     while pending and time() - start < options.timeout:
         pending = list(filter_passing(pending))
@@ -120,6 +122,6 @@ def main():
         if options.no_abort:
             print('Failed checks:', ', '.join(str(check) for check in pending))
         else:
-            parser.error('Failed service checks: %s. Aborting!' % ', '.join(str(check) for check in pending))
+            parser.exit(1, 'Failed service checks: %s. Aborting!' % ', '.join(str(check) for check in pending))
     if command:
         os.execvp(command[0], command)
