33ebcfe27a70ce8722f4d0d164cb187a624cadf0
Dan <ailenei_d@yahoo.com>
2019-05-21 22:14:50 +0300

    Revert "additional check for fatal errors" revert additional error checks This reverts commit 3eb65ef83d1aece97371c921f0f2c8b87c7eefd5.

--- src/holdup/cli.py
+++ src/holdup/cli.py
@@ -56,16 +56,11 @@ class Check(object):
         try:
             self.run(options)
         except Exception as exc:
-            if self.is_fatal_error(exc):
-                raise exc
             self.error = exc
         else:
             if options.verbose:
                 print('holdup: Passed check: %r' % self)
             return True
-
-    def is_fatal_error(self, error):
-        return False
 
     def __repr__(self):
         if self.error:
@@ -97,20 +92,14 @@ class PgCheck(Check):
         self.password = password
         self.port = port
 
-    def run(self, options):
+    def run(self, timeout, _):
         with closing(psycopg2.connect(host=self.host, database=self.database, user=self.user, password=self.password,
-                                      port=self.port, connect_timeout=options.check_timeout)) as conn:
+                                      port=self.port, connect_timeout=timeout)) as conn:
             # create a cursor
             with closing(conn.cursor()) as cur:
                 start = time()
                 cur.execute('SELECT version()')
                 db_version = cur.fetchone()
-
-    def is_fatal_error(self, error):
-        representation = repr(error)
-        if 'password authentication failed' in representation or\
-            'database "{0}" does not exist'.format(self.database) in representation:
-            return True
 
     def __str__(self):
         return 'postgresql://{0.user}:{0.password}:{0.host}:{0.database}'.format(self)
