6fe7704fff109ea1ef3f556c8aed50ebb0fdc2dc
Dan <ailenei_d@yahoo.com>
2019-05-22 00:28:21 +0300

    used uri connection string for pg check instead of parsing text

--- src/holdup/cli.py
+++ src/holdup/cli.py
@@ -21,6 +21,8 @@ import argparse
 import ast
 import os
 import psycopg2
+from psycopg2.extensions import make_dsn
+from psycopg2 import ProgrammingError
 import socket
 import ssl
 import sys
@@ -85,16 +87,11 @@ class TcpCheck(Check):
 
 
 class PgCheck(Check):
-    def __init__(self, host, port, database, user, password):
-        self.host = host
-        self.database = database
-        self.user = user
-        self.password = password
-        self.port = port
+    def __init__(self, connection_string):
+        self.connection_string = connection_string
 
-    def run(self, timeout, _):
-        with closing(psycopg2.connect(host=self.host, database=self.database, user=self.user, password=self.password,
-                                      port=self.port, connect_timeout=timeout)) as conn:
+    def run(self, options):
+        with closing(psycopg2.connect(self.connection_string, connect_timeout=options.check_timeout)) as conn:
             # create a cursor
             with closing(conn.cursor()) as cur:
                 start = time()
@@ -102,7 +99,7 @@ class PgCheck(Check):
                 db_version = cur.fetchone()
 
     def __str__(self):
-        return 'postgresql://{0.user}:{0.password}:{0.host}:{0.database}'.format(self)
+        return self.connection_string
 
 class HttpCheck(Check):
     def __init__(self, url):
@@ -259,12 +256,14 @@ def parse_value(value, proto):
             raise argparse.ArgumentTypeError('Invalid service spec %r. Port must be a number.' % value)
         port = int(port)
         return TcpCheck(host, port)
-    elif proto == 'postgresql':
-        user, password, host, port, dbname = value.strip('/').split(':', 4)
-        if not port.isdigit():
-            raise argparse.ArgumentTypeError('Invalid service spec %r. Port must be a number.' % value)
-        port = int(port)
-        return PgCheck(host, port, dbname, user, password)
+    elif proto in ('pg', 'postgresql', 'postgres'):
+        uri = 'postgresql://{}'.format(value)
+        try:
+            connection_uri = make_dsn(uri)
+        except ProgrammingError:
+            raise ProgrammingError('Connection string "{}" postgres database is invalid, the format is'\
+                                   '"postgresql://user:password@host:port/dbname"'.format(uri))
+        return PgCheck(connection_uri)
     elif proto == 'unix':
         return UnixCheck(value)
     elif proto == 'path':
@@ -285,7 +284,7 @@ parser.add_argument('service', nargs=argparse.ONE_OR_MORE, type=parse_service,
                     help='A service to wait for. '
                          'Supported protocols: "tcp://host:port/", "path:///path/to/something", '
                          '"unix:///path/to/domain.sock", "eval://expr", '
-                         '"postgresql://user:password:host:port:database", '
+                         '"postgresql://user:password@host:port/dbname" '
                          '"http://urn", "http://urn", "https+insecure//urn" (status 200 expected). '
                          'Join protocols with a comma to make holdup exit at the first '
                          'passing one, eg: tcp://host:1,host:2 or tcp://host:1,tcp://host:2 are equivalent and mean '
