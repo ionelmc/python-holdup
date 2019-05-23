621d609a8263dae39057acaec76e5f126c7bfc5c
Ionel Cristian Mărieș <contact@ionelmc.ro>
2019-05-23 03:01:16 +0300

    Retry to see all the range of errors.

--- tests/test_pg.py
+++ tests/test_pg.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    import time
     import traceback
 
     import psycopg2
 
-    try:
-        conn = psycopg2.connect("postgresql://app:app@pg:5432/app")
-        cursor = conn.cursor()
-        cursor.execute("SELECT version()")
-        print(cursor.fetchone())
-    except Exception:
-        traceback.print_exc()
+    start = time.time()
+    failed = False
+
+    while time.time() - start < 10:
+        try:
+            conn = psycopg2.connect("postgresql://app:app@pg:5432/app")
+            cursor = conn.cursor()
+            cursor.execute("SELECT version()")
+            print(cursor.fetchone())
+        except Exception:
+            traceback.print_exc()
+            failed = True
+        else:
+            break
+
+    if failed:
         sys.exit(1)
